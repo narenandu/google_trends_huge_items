8c06cc7ffcc864bcd0c6daa1b2844ed3d659f67a
Narendra Kumar Vadapalli <narenandu@gmail.com>
2019-12-21 20:50:42 -0800

    updated script to pass start and end indices

--- google_trends.py
+++ google_trends.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import pandas as pd
 from pytrends.request import TrendReq
@@ -11,30 +12,39 @@ SEARCH_TERMS = "search_terms.csv"
 start_term_idx = 700
 end_term_idx = 1050
 REF_TERM = "Ounces in Pound"
-OUTPUT_FILE="ounces_to_pounds_vs_7000_google_trends_1050.csv"
+
 
 def get_terms(start_idx=start_term_idx, end_idx=end_term_idx):
     terms_file = os.path.join(here, SEARCH_TERMS)
     terms_df = pd.read_csv(terms_file)
-    act = terms_df[start_idx:end_idx].Act
+    act = terms_df[int(start_idx):int(end_idx)].Act
     print(act.values)
     return act.values
 
-terms = get_terms()
+def main(start, end):
 
-for i, term in enumerate(terms):
-    if i % 25 == 0:
-        print("processing ... i, term: {} -> {}...".format(i, term))
-    time.sleep(1)
-    pytrends.build_payload([REF_TERM, term], timeframe="2015-11-01 2019-12-20", geo='US')
-    df = pytrends.interest_over_time()
-    df_ = df.drop("isPartial", axis=1)
-    if i == 0:
-        df_temp = df_.copy()
-    else:
-        result = pd.concat([df_temp, df_], axis=1)
-        df_temp = result
+    terms = get_terms(start, end)
+    print(terms)
+    for i, term in enumerate(terms):
+        if i % 25 == 0:
+            print("processing ... i, term: {} -> {}...".format(i, term))
+        time.sleep(0.25)
+        pytrends.build_payload([REF_TERM, term], timeframe="2015-11-01 2019-12-20", geo='US')
+        df = pytrends.interest_over_time()
+        df_ = df.drop("isPartial", axis=1)
+        if i == 0:
+            df_temp = df_.copy()
+        else:
+            result = pd.concat([df_temp, df_], axis=1)
+            df_temp = result
 
-output_file = os.path.join(here, OUTPUT_FILE)
-print("writing to csv file")
-df_temp.to_csv(output_file)
+    OUTPUT_FILE="ounces_to_pounds_vs_7000_google_trends_{}.csv".format(end)
+    output_file = os.path.join(here, OUTPUT_FILE)
+    print("writing to csv file")
+    df_temp.to_csv(output_file)
+
+if __name__ == "__main__":
+    start = sys.argv[1]
+    end = sys.argv[2]
+    print(start, end)
+    main(start, end)
